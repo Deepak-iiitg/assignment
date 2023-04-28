03bbfa6e240f19b57c17c57c743667a2d85188fe
Deepak-iiitg <94068572+Deepak-iiitg@users.noreply.github.com>
2023-04-28 14:19:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const submit = document.getElementById("submit");
+submit.addEventListener("click",
+function display(e){
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const date = document.getElementById("date").value;
+    const time = document.getElementById("time").value;
+    console.log(name);
+    console.log(email);
+    console.log(phone);
+    console.log(date);
+    console.log(time);
+})
